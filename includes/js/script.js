ff112f3e3c771dac0357a1a7d2063f0ef77bbd58
Prashant Farkya <prashant.farkya@gmail.com>
2016-07-08 19:33:54 +0530

    bootstrap more feature

--- includes/js/script.js
+++ includes/js/script.js
@@ -9,3 +9,16 @@ Notes:	 Hand coded for Assignment in Stack Route
 
 */
 
+// var header,ypos;
+// window.addeventListener('scroll',function() {
+// 	var topbrand=document.getElementByClassName('');
+// 	var ypos= window.pageYOffset;
+// 	if(ypos>300){
+// 	}	
+
+// 	// body...
+// });
+
+$(window).on("scroll touchmove", function () {
+  $('.shrink').toggleClass('tiny', $(document).scrollTop() > 300);
+});
